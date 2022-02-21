fe6f278e687893cc38ed12b291bc0ef1add78d72
Bella <98971465+Bellawareness@users.noreply.github.com>
2022-02-21 23:19:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+int i =20;
+   do{
+
+System.out.println(i--);
+
+   }while (i>=1); 
+  
+// 1. Create a do-while loop that prints  the numbers from 20 - 1. You can declare an int variable before the do-while loop
+}
+}
